8c3f360bfa0ed93feb1ca79397aabfa5602410b0
Joshua <joshuajacobphiri@gmail.com>
2024-12-23 12:59:00 +0400

    first commit

---
+++ app/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section>
+        <h1 className='text-white mb-4 text-4xl lg:text-6xl font-extrabold'>Hi Joshua</h1>
+        <p className='text-[#ADB7BE] text-lg lg:text-xl'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam labore corporis, debitis unde dolores deserunt magnam fuga nostrum mollitia 
+            eveniet expedita hic eaque quisquam dicta repudiandae aliquid. Unde, distinctio veniam!
+        </p>
+    </section>
+  )
+}
+
+export default Header
